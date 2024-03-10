c78465c422f1ade67ca7882b20691d6c0533c8e0
OscarT99 <oscartorres990926@outlook.com>
2024-03-10 16:16:27 -0500

    MODULOS FINALIZADOS

--- src/app/components/pages/pedido/list.pedido/list.pedido.module.ts
+++ src/app/components/pages/pedido/list.pedido/list.pedido.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ListPedidoRoutingModule } from './list.pedido.routing';  
 import { ListPedidoComponent } from './list.pedido.component';  
 import { TableModule } from 'primeng/table';
@@ -9,6 +10,7 @@ import { RippleModule } from 'primeng/ripple';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { DialogModule } from 'primeng/dialog';
 import { TooltipModule } from 'primeng/tooltip';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -21,11 +23,13 @@ import { TagModule } from 'primeng/tag';
         ListPedidoRoutingModule,
         TableModule,
         FormsModule,
+        ReactiveFormsModule,
         ButtonModule,
         RippleModule,
         ToastModule,
         ToolbarModule,
         InputTextModule,
+        InputTextareaModule,
         DialogModule, 
         TooltipModule,
         ConfirmDialogModule,
